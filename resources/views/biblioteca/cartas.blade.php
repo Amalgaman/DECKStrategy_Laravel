a404ded7aaf92c46ebd40dd67a3c2da3a3fd786d
IgnacioPat <ignacio.patafio@davinci.edu.ar>
2023-08-02 00:37:02 -0300

    cambios ui

--- resources/views/biblioteca/cartas.blade.php
+++ resources/views/biblioteca/cartas.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container mt-5">
     <div class="row">
-        <div class="col text-center">
+        <div class="col text-center p-5">
             <h1>Biblioteca de cartas</h1>
         </div>
     </div>
-    <div class="row justify-content-evenly mt-3">
+    <div class="row justify-content-evenly mt-3 text-center">
     @foreach ($cartas as $carta)
         <div class="col-5 col-lg-2 m-3"><img class="img-fluid" src="{{$carta->img_chica}}" alt="..."></div>
     @endforeach
     </div>
-    <div class="row justify-content-center">
-        <div class="col col-md-5">
+    <div class="row justify-content-center img-fluid p-5">
+        <div class="col col-md-5 pagination justify-content-center">
              {{ $cartas->links() }}
         </div>
     </div>
--- resources/views/misdecks/creador.blade.php
+++ resources/views/misdecks/creador.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid mt-5">
     <div class="row">
         <div class="col text-center">
-            <h1>Creador de Mazo</h1>
+            <h1 class="p-5">Creador de Mazo</h1>
         </div>
     </div>
     <div class="row">
@@ -20,7 +20,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-3" id="deckbuilder">
+        <div class="col-3 p-4" id="deckbuilder" >
         <form action="">
             <input type="text" name="nombre" id="nombre" placeholder="Nombre del Mazo">
             <button type="submit" class="btn btn-primary my-2">Guardar</button>
