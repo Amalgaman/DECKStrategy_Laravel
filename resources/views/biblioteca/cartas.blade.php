1f5e559cccc8eeefb9a517d4456b86ab1baa2b69
IgnacioPat <ignacio.patafio@davinci.edu.ar>
2023-08-03 21:56:37 -0300

    trad y hover

--- resources/views/biblioteca/cartas.blade.php
+++ resources/views/biblioteca/cartas.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    /* Estilo inicial de la imagen */
+    .image-container {
+        overflow: hidden; /* Oculta el contenido que desborda */
+        height: 100%; /* Ajusta la altura del contenedor */
+        display: flex; /* Activa el modo flex para alinear la imagen */
+        justify-content: center; /* Centra la imagen horizontalmente */
+        align-items: center; /* Centra la imagen verticalmente */
+    }
+
+    /* Estilo de la imagen cuando el mouse está sobre ella */
+    .img-fluid:hover {
+        transform: scale(2.0); /* Escala la imagen al 120% */
+        transition: transform 0.3s ease; /* Añade una transición suave */
+    }
+</style>
 <div class="container mt-5">
     <div class="row">
         <div class="col text-center p-5">
@@ -12,7 +28,7 @@
         <div class="col-5 col-lg-2 m-3"><img class="img-fluid" src="{{$carta->img_chica}}" alt="..."></div>
     @endforeach
     </div>
-    <div class="row justify-content-center img-fluid p-5">
+    <div class="row justify-content-center p-5">
         <div class="col col-md-5 pagination justify-content-center">
              {{ $cartas->links() }}
         </div>
