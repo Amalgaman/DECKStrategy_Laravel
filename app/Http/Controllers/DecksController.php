10af77e0cbaf3c307458e19885fd3d501286ab71
Amalgaman <xxxrigobertoxxx@gmail.com>
2023-08-04 15:22:40 -0300

    xd

--- app/Http/Controllers/DecksController.php
+++ app/Http/Controllers/DecksController.php
@@ -13,10 +13,25 @@ class DecksController extends Controller
 {
     public function show(Deck $deck){
 
-        $cartas = Carta::where('id','>',0)
+        $cartas = Cartas::where('id','=',0)
         ->orderBy('nombre')
         ->paginate(10);
 
+/*
+        $lista = lista_deck::where('id','=',$deck->id)
+        ->orderBy('nombre')
+        ->paginate(10);
+
+        foreach($lista as $l){
+            $carta = Carta::where('id','=',$l->id)
+        ->orderBy('nombre')
+        ->paginate(10);
+
+
+
+            $cartas.push($carta);
+        }
+*/
         return view('decks.show',[
             'cartas' => $cartas,
         ]);
