9d0b3e031438723177cf460e79373f557ce391ca
Amalgaman <xxxrigobertoxxx@gmail.com>
2023-08-06 01:30:41 -0300

    sdsd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 p-5">
-            <div class="card">
-              <!--  <div class="card-header">{{ __('Dashboard') }}</div>
-              -->
-                <div class="card-body p-5 centrado shadow-lg">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('¡Iniciaste sesion!') }}
-                </div>
-            </div>
+<div class="contenedor">
+    <div class="seccion">
+        <div>
+            <h3 class="text-center">Mazos Populares</h3>
+        </div>
+        <div class="mazos" style="background-image: url('ruta de imagen'); background-size: cover">
+            <h5>Selesnya Tokens</h5>
+            <p>por Amalgaman</p>
+            <span>Control</span>
+            <span>Dificil</span>
+            <span>Meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Boros aggro</h5>
+            <p>por Amalgaman</p>
+            <span>Aggro</span>
+            <span>Fácil</span>
+            <span>meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Dimir Midrange</h5>
+            <p>por Amalgaman</p>
+            <span>Midrange</span>
+            <span>Combo</span>
+            <span>Meta</span>
         </div>
     </div>
-</div>
+    <div class="seccion" style="">
+    <div class="" style="">
+            <h3 class="text-center">Mazos Profesionales</h3>
+        </div>
+        <div class="mazos">
+            <h5>Boros aggro</h5>
+            <p>por Amalgaman</p>
+            <span>Aggro</span>
+            <span>Fácil</span>
+            <span>meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Dimir Midrange</h5>
+            <p>por Amalgaman</p>
+            <span>Midrange</span>
+            <span>Combo</span>
+            <span>Meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Selesnya Tokens</h5>
+            <p>por Amalgaman</p>
+            <span>Control</span>
+            <span>Dificil</span>
+            <span>Meta</span>
+        </div>
+    </div>
+    <div class="seccion">
+    <div class="">
+            <h3 class="text-center">Mazos Meme</h3>
+        </div>
+        <div class="mazos">
+            <h5>Dimir Midrange</h5>
+            <p>por Amalgaman</p>
+            <span>Midrange</span>
+            <span>Combo</span>
+            <span>Meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Boros aggro</h5>
+            <p>por Amalgaman</p>
+            <span>Aggro</span>
+            <span>Fácil</span>
+            <span>meta</span>
+        </div>
+        <div class="mazos">
+            <h5>Selesnya Tokens</h5>
+            <p>por Amalgaman</p>
+            <span>Control</span>
+            <span>Dificil</span>
+            <span>Meta</span>
+        </div>
+    </div>
+
+    <div id="info">
+        <h1>Quienes somos</h1>
+        <p>Somos una empresa llena de gente apasionada por los videojuegos y los juegos de mesa, en especial los juegos de cartas. Nuestro objetivo es fomentar una gran comunidad de jugadores donde todos puedan ayudarse y mejorar. Como planes a futuro nos encantaría poder facilitar la organización de torneos y expandirnos a mercados aún más grandes para aumentar la base de jugadores. </p>
+    </div>
+    </div>
 @endsection
